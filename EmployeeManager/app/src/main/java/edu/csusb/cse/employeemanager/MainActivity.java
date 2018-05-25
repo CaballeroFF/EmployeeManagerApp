7de5000312bddf3432f515d5d5e8ff99ea57ee34
CaballeroFF <CaballeroFF@users.noreply.github.com>
2018-05-25 14:00:26 -0700

    alert dialog
    created alert dialog to get text

--- EmployeeManager/app/src/main/java/edu/csusb/cse/employeemanager/MainActivity.java
+++ EmployeeManager/app/src/main/java/edu/csusb/cse/employeemanager/MainActivity.java
@@ -1,12 +1,24 @@
 package edu.csusb.cse.employeemanager;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.support.design.widget.Snackbar;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DEBUG";
+
+    final Context context = this;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +26,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button dialogButton = findViewById(R.id.dialog_test);
+        textView = findViewById(R.id.test_text);
 
         dialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showPrompt();
                 Snackbar.make(v, "Clicked", Snackbar.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void showPrompt(){
+        //get prompt.xml
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View promptView = layoutInflater.inflate(R.layout.prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptView);
+
+        final EditText userInput = promptView.findViewById(R.id.editTextDialogUserInput);
+
+        //set dialog message
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //todo: save it to a string
+                                textView.setText(userInput.getText().toString());
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+        //create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        //show alert dialog
+        alertDialog.show();
     }
 }
